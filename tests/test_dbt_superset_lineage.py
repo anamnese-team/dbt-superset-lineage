c425bca9f3ab48b4e1746f540d33c6693e85ce01
Omar EL OUBARI <omar.oubari@anamnese.care>
2026-04-15 03:24:54 +0200

    feat: update dependencies

--- tests/test_dbt_superset_lineage.py
+++ tests/test_dbt_superset_lineage.py
@@ -1,4 +1,9 @@
+from dbt_superset_lineage import __version__
 from dbt_superset_lineage.push_descriptions import extract_certification_from_table
+
+
+def test_version():
+    assert __version__ == '0.4.0'
 
 
 def test_extract_certification_with_nested_meta_and_freshness():
